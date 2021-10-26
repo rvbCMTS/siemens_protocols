3fa89aa8f92d64fb05bd85ed3e2acc2ea75dbaa9
psvenmarker <pontus.svenmarker@vll.se>
2021-10-26 13:04:20 +0200

    Handles multiple sqlite database versions. Removed support for mdb files.

--- organ_programs_dbv45.sql
+++ organ_programs_dbv45.sql
@@ -1,5 +1,7 @@
 SELECT 	OGP.Name AS ris_name,
+		Exam.Name AS exam_name,
 		BodyPart.Name AS body_part,
+		AcquisitionSystem.Name AS acquisition_system,
 		Technique.Value AS technique,
 		OGP_kV.Value AS kv,
 		RADOGP_mAs.Value AS mas,
@@ -14,10 +16,14 @@ SELECT 	OGP.Name AS ris_name,
 		RAD_OGP.Imageautoamplification AS image_auto_amplification,
 		ImageAmplification.Value AS image_amplification_gain,
 		Dose_RAD.Sensitivity AS sensitivity,
-		GradationParameter.Name AS lut
+		GradationParameter.Name AS lut,
+		FPSet.Name AS fp_set
 FROM OGP
 
+INNER JOIN Exam_OGP ON OGP.Id = Exam_OGP.Id_ogp
+INNER JOIN Exam ON Exam.Id = Exam_OGP.Id_exam
 INNER JOIN BodyPart ON BodyPart.Id = OGP.Id_bodypart
+INNER JOIN AcquisitionSystem ON AcquisitionSystem.Id = OGP.Id_acqsystem
 INNER JOIN OGP_kV  ON OGP_kV.ID = OGP.ID_kV
 INNER JOIN RAD_OGP ON RAD_OGP.ID = OGP.ID
 LEFT OUTER JOIN RADOGP_mAs ON RAD_OGP.Id_mas = RADOGP_mAs.Id
@@ -31,6 +37,13 @@ INNER JOIN HarmonisKernel ON SpatialFrequencyParameter.Id_harmoniskernel = Harmo
 LEFT OUTER JOIN ImageAmplification ON RAD_OGP.Id_imageamplification = ImageAmplification.Id
 INNER JOIN Dose_RAD ON RAD_OGP.Id_dose = Dose_RAD.Id
 INNER JOIN GradationParameter ON RAD_OGP.Id_imagegradation = GradationParameter.Ids
+LEFT OUTER JOIN FPSet ON FPset.ID = OGP.ID_FPSet
 
 
-WHERE OGP.status=2 AND OGP.Type='SIEMENSDEFAULT';
+WHERE
+OGP.status=2
+AND OGP.Type='SIEMENSDEFAULT'
+AND NOT OGP.Id_bodypart=57
+AND ris_name NOT LIKE '-%'
+AND ris_name NOT LIKE '. _%'
+AND ris_name NOT LIKE '%\_%' ESCAPE '\'
