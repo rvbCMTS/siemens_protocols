99be051b01efcf497f7eb653228346eadae0a4f5
psvenmarker <pontus.svenmarker@vll.se>
2022-12-14 14:54:28 +0100

    Zip-file support and command line input_directory

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from parse_pex_db import parse_pex_db
 import os
+import sys
+from zipfile import ZipFile
 
+# input_directory from command line
+input_directory = sys.argv[1]
+print(f'input_directory: {input_directory}')
 
-# for all mdb files in folder databases
-for root, dirs, files in os.walk('databases'):
+# find all .zip in input_directory
+for root, dirs, files in os.walk(input_directory):
     for file in files:
-        if file.endswith(".sqlite"):
-            print(file)
+        if file.lower().endswith(".zip"):
+            # open zip
+            with ZipFile(os.path.join(root, file), 'r') as z:
+                # list of zips with .sqlite file
+                file_name = [s for s in z.namelist() if '.sqlite' in s]
+                z.extract(file_name[0], root)
 
-            # parse temporary sqlite database
-            df = parse_pex_db(os.path.join(root, file))
+# find all .sqlite in input_directory
+for root, dirs, files in os.walk(input_directory):
+    for file in files:
+        if file.lower().endswith(".sqlite"):
+                print(file)
+                # parse temporary sqlite database
+                df = parse_pex_db(os.path.join(root, file))
 
-            # save dataframe as csv
-            [pre, ext] = os.path.splitext(file)
-            csv_path = os.path.join(root, pre + ".csv")
-            df.to_csv(csv_path)
+                # save dataframe as csv
+                [pre, ext] = os.path.splitext(file)
+                csv_path = os.path.join(root, pre + ".csv")
+                df.to_csv(csv_path)
+
