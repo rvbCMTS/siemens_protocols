b85f857249746649007094e4976e2dd3460435f0
psvenmarker <pontus.svenmarker@regionvasterbotten.se>
2023-10-31 12:26:37 +0100

    C-arm support

---
+++ c_arm_dbv206.sql
@@ -0,0 +1,66 @@
+SELECT Application.name as Application,
+       Application.Rotation,
+       Application.EdgeEnhancementHigh,
+       Application.EdgeEnhancementLow,
+       Application.MotionDetectionHigh,
+       Application.MotionDetectionLow,
+       Application.ViewFlipX,
+       Application.ViewFlipY,
+       Application.MeasurementField,
+       Application.Magnification,
+       Application.AveragingLiveHigh,
+       Application.AveragingLiveLow,
+       Application.SpatialNoiseHigh,
+       Application.SpatialNoiseLow,
+       Application.SetBlack,
+       Application.CenterShift,
+       Application.SpotSize,
+       Application.WidthFactor,
+       Application.ZoomDoseFactor,
+       AcquisitionModeConfig.Name as AcqusitionMode,
+       AcquisitionModeConfig.AutoStore,
+       AcquisitionModeConfig.StorageRate,
+       AcquisitionModeConfig.MaxSceneLength,
+       AcquisitionModeConfig.AutoLoop,
+       AcquisitionModeConfig.NumFramesMask,
+       AcquisitionModeConfig.AutoWindow,
+       AcquisitionModeConfig.LIHStore,
+       Triplet.name as Triplet,
+       TripletElement.DoseOccurrence,
+       TripletElement.Phase,
+       TripletElement.FrameRate,
+       TripletElement.DoseLevel,
+       TripletElement.DoseRateIndex,
+       TripletElement.NumImages,
+       TripletElement.IsDefault,
+       FluoroCurve.Name as FluoroCurve,
+       FluoroCurve.IsHigh,
+       NoiseSpatialFilterParameter.Name as NoiseSpatialFilterParameter,
+       NoiseSpatialFilterParameter.Level,
+       NoiseSpatialFilterParameter.Range,
+       NoiseSpatialFilterParameter.LevelStd,
+       NoiseTimeFilterParameter.Name as NoiseTimeFilterParameter,
+       NoiseTimeFilterParameter.KMin,
+       NoiseTimeFilterParameter.KMax,
+       NoiseTimeFilterParameter.Threshold,
+       NoiseTimeFilterParameter.MotionDetectionOn,
+       SpatialFrequencyResponseParameter.Name as SpatialFrequencyResponseParameter,
+       SpatialFrequencyResponseParameter.EdgeFilterGain,
+       SpatialFrequencyResponseParameter.EdgeFilterKernel,
+       SpatialFrequencyResponseParameter.HarmonisationGain,
+       SpatialFrequencyResponseParameter.HarmonisationKernel,
+       OrbitalScanConfig.Name as OrbitalScanConfig,
+       AngularScanConfig.Name as AngularScanConfig,
+       BasicRecoSet.Name AS BasicRecoSet
+from Application
+
+JOIN AcquisitionModeConfig on Application.ID_AcqModeConfigFluoro=AcquisitionModeConfig.ID
+JOIN Triplet on AcquisitionModeConfig.ID_Triplet=Triplet.ID
+JOIN TripletElement on TripletElement.ID_Triplet=Triplet.ID
+JOIN FluoroCurve on TripletElement.ID_FluoroCurve=FluoroCurve.ID
+JOIN NoiseSpatialFilterParameter on TripletElement.ID_NoiseSpatialFilterParameter=NoiseSpatialFilterParameter.ID
+JOIN NoiseTimeFilterParameter on TripletElement.ID_NoiseTimeFilterParameter=NoiseTimeFilterParameter.ID
+JOIN SpatialFrequencyResponseParameter on TripletElement.ID_SpatialFrequencyResponseParameter=SpatialFrequencyResponseParameter.ID
+LEFT JOIN OrbitalScanConfig on TripletElement.ID_OrbitalScanConfig=OrbitalScanConfig.ID
+LEFT JOIN AngularScanConfig on TripletElement.ID_AngularScanConfig=AngularScanConfig.ID
+LEFT JOIN BasicRecoSet on TripletElement.ID_BasicRecoSet=BasicRecoSet.ID
--- parse_pex_db.py
+++ parse_pex_db.py
@@ -1,46 +1,45 @@
 import sqlite3
 import pandas
-
+from pathlib import Path
 
 def parse_pex_db(path_to_db):
     # Open database
-    conn = sqlite3.connect(path_to_db)
+    with sqlite3.connect(path_to_db) as conn:
 
-    # Find database version
-    sql_db_version = """SELECT * FROM GlobalVars
-                        WHERE GlobalVars.Name = 'DBVersion'"""
-    global_vars = pandas.read_sql_query(sql_db_version, conn)
-    db_version = float(global_vars.iloc[:, 1][0])
+        # fetch all tables
+        cur = conn.cursor()
+        tables_sql = cur.execute(
+            """SELECT name FROM sqlite_master WHERE type='table'; """).fetchall()
+        tables = [t[0] for t in tables_sql]
 
-    # call correct sql-query
-    if 45 <= db_version < 60:
-        fd = open('organ_programs_dbv45.sql', 'r')
-        sql_organ_programs = fd.read()
-        fd.close()
-        # Read from db to dataframe
-        df = pandas.read_sql_query(sql_organ_programs, conn)
-        # Add None for grid
-        df['grid'] = [float('nan')] * len(df)
-    elif 60 <= db_version < 72.01:
-        fd = open('organ_programs_dbv60.sql', 'r')
-        sql_organ_programs = fd.read()
-        fd.close()
-        # Read from db to dataframe
-        df = pandas.read_sql_query(sql_organ_programs, conn)
-    elif 72.01 <= db_version < 74.06:
-        fd = open('organ_programs_dbv7201.sql', 'r')
-        sql_organ_programs = fd.read()
-        fd.close()
-        # Read from db to dataframe
-        df = pandas.read_sql_query(sql_organ_programs, conn)
-    elif 74.06 <= db_version:
-        fd = open('organ_programs_dbv7406.sql', 'r')
-        sql_organ_programs = fd.read()
-        fd.close()
-        # Read from db to dataframe
-        df = pandas.read_sql_query(sql_organ_programs, conn)
+        # Find database version
+        db_version_sql = cur.execute("SELECT varvalue as db_version FROM GlobalVars WHERE name='DBVersion'").fetchall()
+        db_version = float(db_version_sql[0][0])
 
-    # close database
-    conn.close()
+        # modification date
+        modified_sql = cur.execute("""
+            SELECT
+            DATETIME(ROUND(LastModification / 1000), 'unixepoch', 'localtime') as modified
+            FROM GlobalData""").fetchall()
+        modified = modified_sql[0][0]
 
+        if 'OGP' in tables:
+            # call correct sql-query
+            if 45 <= db_version < 60:
+                # Read from db to dataframe
+                df = pandas.read_sql_query(Path('organ_programs_dbv45.sql').read_text(), conn)
+                # Add None for grid
+                df['grid'] = [float('nan')] * len(df)
+            elif 60 <= db_version < 72.01:
+                df = pandas.read_sql_query(Path('organ_programs_dbv60.sql').read_text(), conn)
+            elif 72.01 <= db_version < 74.06:
+                df = pandas.read_sql_query(Path('organ_programs_dbv7201.sql').read_text(), conn)
+            elif 74.06 <= db_version:
+                df = pandas.read_sql_query(Path('organ_programs_dbv7406.sql').read_text(), conn)
+
+        if 'Triplet' in tables:
+            if 20.6 <= db_version:
+                df = pandas.read_sql_query(Path('c_arm_dbv206.sql').read_text(), conn)
+
+        df['modified'] = modified
     return df
